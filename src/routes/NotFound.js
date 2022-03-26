b870a5ecce1ddb069f68d6620f12c05ccc0575a5
Mark Ayvaz <mark.ayvaz98@gmail.com>
2022-03-26 08:49:23 +0100

    Updated 404 artwork

--- src/routes/NotFound.js
+++ src/routes/NotFound.js
@@ -3,21 +3,21 @@ import NotFoundArt from "../assets/svgs/NotFoundArt.svg";
 
 export default function NotFound() {
   return (
-    <>
+    <div>
       <img
         src={NotFoundArt}
         alt="404 - The page you're looking for could not be found."
-        width={400}
+        width={350}
       />
 
       <h3>Oops, there's nothing here...</h3>
-      <p>
+      <p className="mt-2">
         Let's take you back{" "}
-        <Link to="/" className="font-bold text-blue-700">
+        <Link to="/" className="font-medium text-blue-700">
           home
         </Link>
         .
       </p>
-    </>
+    </div>
   );
 }

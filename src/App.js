e152f769a985478d9452947371ffe78c3e3e8b63
Mark Ayvaz <mark.ayvaz98@gmail.com>
2022-03-27 08:00:12 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,10 @@ import { Outlet } from "react-router-dom";
 import DefaultLayout from "./components/layouts/DefaultLayout";
 import { TriviaState, initialTriviaState } from "./context/TriviaState";
 import { useState, useEffect } from "react";
-import { getTrivia } from "./api/OpenTableDB";
 
 export default function App() {
   const [triviaState, setTriviaState] = useState(initialTriviaState);
   const triviaValue = { triviaState, setTriviaState };
-
-  useEffect(() => {
-    getTrivia(triviaState, setTriviaState);
-  }, []);
 
   return (
     <TriviaState.Provider value={triviaValue}>
